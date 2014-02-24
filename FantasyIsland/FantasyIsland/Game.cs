f247e606fca45be52af37d74c07b6aa52833c798
georgikostadinov <georgikostadinov@yahoo.com>
2014-02-24 23:31:09 +0200

    Small bugs fixed

--- FantasyIsland/FantasyIsland/Game.cs
+++ FantasyIsland/FantasyIsland/Game.cs
@@ -1,6 +1,7 @@
 ﻿namespace FantasyIsland
 {
     using System;
+    using System.Collections.Generic;
 
     public static class Game
     {
@@ -9,10 +10,20 @@
             // TODO Menu and implement IIntro to it(Intro to the game - story, goal, etc.)
             Hero myHero = new Hero(PlayerStats.Human, Armor.Medium, Weapon.Sword, SuperPower.DoubleAttack);
             DarkForestLevel forest = new DarkForestLevel(Difficulty.Easy, myHero);
-            forest.Start();
+            //forest.Start();
 
-            //ZombieMountain mountain = new ZombieMountain(Difficulty.Easy, myHero);
+            ZombieMountain mountain = new ZombieMountain(Difficulty.Easy, myHero);
             //mountain.Start();
+
+            //Using polimorphism
+            List<Level> levels = new List<Level>();
+            levels.Add(forest);
+            levels.Add(mountain);
+            foreach (var level in levels)
+            {
+                level.Start();
+            }
+
         }
     }
 }
