8b767b90a5e3b3d7466f62a8adeb3a8dd5d446f4
kskondov <kskondov@gmail.com>
2014-02-26 15:23:09 +0200

    Start screen added

--- FantasyIsland/FantasyIsland/Game.cs
+++ FantasyIsland/FantasyIsland/Game.cs
@@ -6,6 +6,7 @@
     {
         public static void Main()
         {
+            StartScreen.Screen();
             // TODO Menu and implement IIntro to it(Intro to the game - story, goal, etc.)
             Hero myHero = new Hero(PlayerStats.Human, Armor.Medium, Weapon.Sword, SuperPower.DoubleAttack);
             //DarkForestLevel forest = new DarkForestLevel(Difficulty.Easy, myHero);
